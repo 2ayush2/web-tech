10ea3d06ec3182d073dc54cd13b34398b3bee089
unknown <ayushkhadka410@gmail.com>
2023-09-22 07:05:44 +0545

    Lab 1 no 3 finished

---
+++ Lab-1(part-3)/userregestration.js
@@ -0,0 +1,24 @@
+const regestrationForm=document.getElementById('regestration-form');
+const userTable=document.getElementById('user-table');
+
+regestrationForm.onsubmit=(event)=>{
+    document.getElementById('tableData').style.display='block';
+
+
+
+//create a new table row
+
+const newrow = userTable.insertRow(-1)
+const username =newrow.insertCell(0);
+const email =newrow.insertCell(1);
+
+//insert value
+username.innerHTML=document.getElementById('username').value;
+email.innerHTML = document.getElementById('email').value;
+
+return false;
+
+
+
+
+}
