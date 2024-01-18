7fd828d28bd29a70bedee5cd4e2a39b9566b61ca
unknown <ayushkhadka410@gmail.com>
2024-01-18 16:42:04 +0545

    Changed data

--- Lab-1(part-3)/userregestration.js
+++ Lab-1(part-3)/userregestration.js
@@ -1,25 +1,18 @@
-const regestrationForm=document.getElementById('regestration-form');
-const userTable=document.getElementById('user-table');
+const regestrationForm = document.getElementById("regestration-form");
+const userTable = document.getElementById("user-table");
 
-regestrationForm.onsubmit=(event)=>{
-    document.getElementById('tableData').style.display='block';
+regestrationForm.onsubmit = (event) => {
+  document.getElementById("tableData").style.display = "block";
 
+  //create a new table row
 
+  const newrow = userTable.insertRow(-1);
+  const username = newrow.insertCell(0);
+  const email = newrow.insertCell(1);
 
-//create a new table row
+  //insert value
+  username.innerHTML = document.getElementById("username").value;
+  email.innerHTML = document.getElementById("email").value;
 
-const newrow = userTable.insertRow(-1)
-const username =newrow.insertCell(0);
-const email =newrow.insertCell(1);
-
-//insert value
-username.innerHTML=document.getElementById('username').value;
-email.innerHTML = document.getElementById('email').value;
-
-
-return false;
-
-
-
-
-}
+  return false;
+};
